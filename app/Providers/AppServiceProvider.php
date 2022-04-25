9fca633b385594138fa56f592ff06f8990160417
Abbas Momeni <abbasmomeny1994@gmail.com>
2022-04-25 10:31:30 +0430

    laravel 9 initi with changing in users table columns 1400-02-03

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->setDefaultStringLenght();
+    }
+
+    private function setDefaultStringLenght(): void
+    {
+        Schema::defaultStringLength(191);
+    }
+}
---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('mobile', 13)->unique();
+            $table->string('email')->unique();
+
+            $table->string('name');
+            $table->string('password');
+
+            $table->string('avatar');
+            $table->string('website');
+            $table->string('verify_code');
+            $table->timestamp('verified_at')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
